23367f851712e1189d9d5b11389792d2734a4251
Ali Karababa <karababa.alii@gmail.com>
2021-03-20 14:26:39 +0300

    Initial Commit

---
+++ GuessTheNumber/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GuessTheNumber
+//
+//  Created by Ali Karababa on 20.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
